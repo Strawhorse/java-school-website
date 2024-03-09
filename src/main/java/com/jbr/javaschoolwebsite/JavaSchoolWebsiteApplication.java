3451110035692787744e54b43bd1bb0f0445158b
Sean B <strawhorse@gmail.com>
2024-03-09 18:49:54 +0000

    Demo working, had some issues with a login page and some issues with default security

--- src/main/java/com/jbr/javaschoolwebsite/JavaSchoolWebsiteApplication.java
+++ src/main/java/com/jbr/javaschoolwebsite/JavaSchoolWebsiteApplication.java
@@ -2,17 +2,20 @@ package com.jbr.javaschoolwebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class JavaSchoolWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSchoolWebsiteApplication.class, args);
 	}
+
 
 	@GetMapping("/hello")
 	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name){
