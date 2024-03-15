7a791f0f671c2ea95c187825b48f312b161083c3
Sean B <strawhorse@gmail.com>
2024-03-15 13:10:31 +0000

    Created second test endpoint to ensure current setup works

--- src/main/java/com/jbr/javaschoolwebsite/JavaSchoolWebsiteApplication.java
+++ src/main/java/com/jbr/javaschoolwebsite/JavaSchoolWebsiteApplication.java
@@ -10,23 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
-public class JavaSchoolWebsiteApplication {
+public abstract class JavaSchoolWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSchoolWebsiteApplication.class, args);
 	}
 
 	@GetMapping("/hello")
-	public String sayHello(@RequestParam(value = "myName", defaultValue = "Goran") String name){
+	public String sayHello(@RequestParam(value = "myName", defaultValue = " there...") String name){
 		return String.format("Hello %s!",name);
 	}
 
 
-//	Begin mapping web pages here to serve content
-	@GetMapping("/home")
-	public showIndex() {
-		return index.html;
+//	test endpoint
+	@GetMapping("/test")
+	public String test() {
+		return new String("Hello there. \n This is a test.");
 	}
+
+
+
+
+//	Spring will automatically serve index.html from main/resrouces/static as the homepage
+
+
 
 
 }
